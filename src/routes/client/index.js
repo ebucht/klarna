62a9420c95529d013e3b6da5b913b81f8c0e3fc6
Eddie Bucht <bucht0289@gmail.com>
2022-05-13 16:22:43 +0200

    make it better

---
+++ src/routes/client/index.js
@@ -0,0 +1,14 @@
+const app = require('../../loaders/express-handlebars');
+const { createOrder } = require('../../services/server/klarna');
+const { getItemById } = require('../../services/server/fakeStore');
+
+app.get('/checkout/:product_id', async function (req, res, next) {
+	const { product_id } = req.params;
+	const product = await getItemById(product_id);
+	const products = [{ product, quantity: 1 }];
+	const klarnaJsonResponse = await createOrder(products);
+	const html_snippet = klarnaJsonResponse.html_snippet;
+	res.render('checkout', { klarna_checkout: html_snippet });
+});
+
+module.exports = app;
