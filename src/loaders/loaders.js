c1344afe35f2efb500181bee9e221fa89488629a
Eddie Bucht <bucht0289@gmail.com>
2022-05-27 14:48:56 +0200

    small changes

--- src/loaders/loaders.js
+++ src/loaders/loaders.js
@@ -2,7 +2,6 @@
 require('dotenv').config();
 
 // Import Express and initialize Express Server Application
-const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 
@@ -11,15 +10,13 @@ app.use(require('./express-handlebars'));
 
 // Make all files in the /public folder accessible to everyone,
 // so that we can show images in the browser for example
-app.use('../public', express.static('public'));
+app.use(express.static('public'));
 
 app.use(require('./routes'));
 
 /**
  * 	Start Express Server Application
  **/
-app.listen(PORT, function () {
-	console.log(`App listening on port ${PORT}`);
-});
+app.listen(process.env.PORT);
 
 module.exports = app;
